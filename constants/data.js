34211b9c123019ab74b1bdfaa8f522780379e317
juwatow <juwatow@gmail.com>
2022-11-02 21:33:46 -0400

    Added resume - Config prettier eslint

--- constants/data.js
+++ constants/data.js
@@ -25,7 +25,7 @@ const userData = {
         ` My careeer journey can be summarized into 3 phases:`,
         ` Phase I | 2007-2017: like Camille Fournier describes it very well in her article “The Senior Shift”, this phase was about me growing from someone who needed a lot of oversight to an independent engineer. I focused on developing the best practices and pushing high quality code. That led me to own bigger and more complex projects and show that I was capable, independent, and trustworthy (some examples: added Solr to improve the search functionality, added Redis as our caching system to improve the performance, extended the authentication service with Identity Providers, SAML SSO Authentication, 0Auth and OpenId).`,
         ` Phase II | 2017-2022: This phase was about increasing the scope of ownership, delivery, and impact. Started by leading the first product team where the EPD triad partnership operated in the way described by Marty Cagan (PM: viable & valuable, Design: Usable, ENG: Feasible), experimenting with new ways of delivering a feature which resulted in releasing ahead of time and the team became an inspiration to other teams in the company. This phase was marked by developing leadership skills, guiding people through coaching, mentoring and sponsorship, implementing strategies to cope with industry challenges like hiring, compensation, onboarding, retaining and attrition.`,
-        ` Phase III | 2022-Now - Engineering Leader: From my journey so far I have come to realize that any saas company will go through some transitions. While these transitions are inevitable, I am challenging myself to make them as smooth as possible for people and the organization by guiding them in putting quickly in place the adequate processes, habits and mindset for each step of the transition.`
+        ` Phase III | 2022-Now: From my journey so far I have come to realize that any SaaS company will go through multiple transitions, for example: a successful feature launch or partnership that brings thousands or millions new users and requires the company to scale the team and the application. While these transitions are inevitable, I am challenging myself to make them as smooth as possible for the organization and people behind it by guiding them in putting quickly in place the adequate processes, habits/rituals and mindset for each transition.`
       ],
       currentProject: "PD Day Planner",
       currentProjectUrl: "https://jeannedarc.io/pddp",
@@ -47,7 +47,7 @@ const userData = {
       },
     ],
     resumeUrl:
-      "https://jeannedarc.io/jeanne-resume.pdf",
+      "/jeanne-resume.pdf",
     socialLinks: {
       instagram: "https://instagram.com/juwatow",
       twitter: "https://twitter.com/juwatow",
